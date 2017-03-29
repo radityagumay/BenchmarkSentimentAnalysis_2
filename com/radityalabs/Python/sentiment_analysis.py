052c7ebd45f63bd5bb9cb6409c0481bc7ed5479d
radityagumay <gumay.raditya@yahoo.com>
2017-03-29 17:51:52 +0700

    add feature to file

--- com/radityalabs/Python/sentiment_analysis.py
+++ com/radityalabs/Python/sentiment_analysis.py
@@ -11,11 +11,12 @@ conn = pymysql.connect(
 
 cur = conn.cursor()
 
-cur.execute("SELECT reviewBody,label FROM review_label_benchmark_with_polarity")
+cur.execute("SELECT reviewBody,label FROM review_label_benchmark_with_polarity limit 50")
 
 training_data = []
 
 for r in cur:
+    print(r)
     training_data.append(r)
 
 vocabulary = set(chain(*[word_tokenize(i[0].lower()) for i in training_data]))
@@ -23,14 +24,21 @@ vocabulary = set(chain(*[word_tokenize(i[0].lower()) for i in training_data]))
 feature_set = [({i: (i in word_tokenize(sentence.lower())) for i in vocabulary}, tag) for sentence, tag in
                training_data]
 
-classifier = nbc.train(feature_set)
+file = open("feature.txt", "w")
+file.write(feature_set)
 
-test_sentence = "This is the best band I've ever heard!"
+local_feature = open("feature.txt", "r")
+
+classifier = nbc.train(file.read())
+
+test_sentence = "Twitter Great & Fun app to have!!"
 featurized_test_sentence = {i: (i in word_tokenize(test_sentence.lower())) for i in vocabulary}
 
 print("test_sent:", test_sentence)
 print("tag:", classifier.classify(featurized_test_sentence))
 
+file.close()
+local_feature.close()
 cur.close()
 conn.close()
 
