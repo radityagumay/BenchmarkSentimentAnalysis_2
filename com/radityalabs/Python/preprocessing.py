32cfec00ecc3643bfb7ee3d9e9a64e28567bce82
radityagumay <gumay.raditya@yahoo.com>
2017-04-01 21:38:55 +0700

    fix preprocessing

--- com/radityalabs/Python/preprocessing.py
+++ com/radityalabs/Python/preprocessing.py
@@ -5,6 +5,14 @@ import codecs, sys, glob, os, unicodedata
 
 text = "hello world, i am good"
 
+# when we load data from database, we should consifer use below approach to make
+# data preprocessing
+# 1. load data from database
+# 2. tokenized
+# 3. Stemming
+# 4. punctuation
+# 5. stopword
+
 tokenized = word_tokenize(text)
 
 stemmer = EnglishStemmer()
@@ -13,36 +21,31 @@ stemmer = EnglishStemmer()
 tbl = dict.fromkeys(i for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P'))
 
 
-# Create function with which to strip punct from unicode
-# def remove_punctuation_2(term):
-#     return term.translate(tbl)
-
-
 def is_string_empty(string):
     if string == "":
         return False
     return True
 
 
+sentence = ""
+
 for i in tokenized:
-    stem = stemmer.stem(i)
-    punc = stem.translate(tbl)
-    if is_string_empty(punc):
-        stop = punc not in set(stopwords.words('english'))
-        print(stop, punc)
+    lower = i.lower()  # toLower().Case
+    if len(lower) > 3:  # only len > 3
+        stem = stemmer.stem(lower)  # root word
+        punc = stem.translate(tbl)  # remove ? ! @ etc
+        if is_string_empty(punc):  # check if not empty
+            stop = punc not in set(stopwords.words('english'))
+            if stop:  # only true we append
+                sentence += str(punc) + " "
+
+print(sentence)
 
 
 
 # Define english stopword
 # def stop_word_2(term):
 #     return term not in set(stopwords.words('english'))
-
-
-# when we load data from database, we should consifer use below approach to make
-# data preprocessing
-# 1. load data from database
-# 2. remove punctuation
-# 3. define stopword
 
 
 # sentence = "this is a foo bar sentence"
