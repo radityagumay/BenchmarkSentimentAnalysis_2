d12dcb1bef8830643e2d9b02eaca2db5db7da811
radityagumay <gumay.raditya@yahoo.com>
2017-04-02 23:19:39 +0700

    add progress

--- com/radityalabs/Python/preprocessing2.py
+++ com/radityalabs/Python/preprocessing2.py
@@ -9,14 +9,14 @@ import _pickle as cPickle
 import sys, unicodedata
 import pymysql
 
-conn = pymysql.connect(
-    host='127.0.0.1',
-    user='root', passwd='',
-    db='sentiment_analysis')
-
-cur = conn.cursor()
-
-cur.execute("SELECT reviewBody, label FROM review_label_benchmark_with_polarity")
+# conn = pymysql.connect(
+#     host='127.0.0.1',
+#     user='root', passwd='',
+#     db='sentiment_analysis')
+#
+# cur = conn.cursor()
+#
+# cur.execute("SELECT reviewBody, label FROM review_label_benchmark_with_polarity")
 
 stemmer = EnglishStemmer()
 
@@ -50,77 +50,80 @@ posids = movie_reviews.fileids('pos')
 negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
 posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
 
-vocabulary = {}
-
-negbar = progressbar.ProgressBar(maxval=len(negids), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-negbar.start()
-index = 0
-for i in negids:
-    negbar.update(index + 1)
-    for j in movie_reviews.words(fileids=[i]):
-        is_valid_vocab = preprocessing(j)
-        if is_string_not_empty(is_valid_vocab):
-            if j not in vocabulary:
-                vocabulary[j] = j
-    index += 1
-
-print("\n Done added negative movie vocabulary : ", len(vocabulary))
-print("\n")
-
-posbar = progressbar.ProgressBar(maxval=len(posids), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-posbar.start()
-index = 0
-for i in posids:
-    posbar.update(index + 1)
-    for j in movie_reviews.words(fileids=[i]):
-        is_valid_vocab = preprocessing(j)
-        if is_string_not_empty(is_valid_vocab):
-            if j not in vocabulary:
-                vocabulary[j] = j
-    index += 1
-
-print("Done added positive movie vocabulary : ", len(vocabulary))
-print("\n")
-
-db_count = 64757
-dbbar = progressbar.ProgressBar(maxval=db_count, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-dbbar.start()
-index = 0
-for i in cur:
-    dbbar.update(index + 1)
-    words = word_tokenize(i[0])
-    for j in words:
-        is_valid_vocab = preprocessing(j)
-        if is_string_not_empty(is_valid_vocab):
-            if j not in vocabulary:
-                vocabulary[j] = j
-    index += 1
-
-print("Done added database movie vocabulary : ", len(vocabulary))
-print("\n")
-
-cur.close()
-conn.close()
-
-with open("vocabulary.pickle", "wb") as handle:
-    cPickle.dump(vocabulary, handle)
-
-print(vocabulary)
-
-
-# negcutoff = len(negfeats) * 3 / 4
-# poscutoff = len(posfeats) * 3 / 4
+# vocabulary = {}
 #
-# trainfeats = negfeats[:int(negcutoff)] + posfeats[:int(poscutoff)]
-# testfeats = negfeats[int(negcutoff):] + posfeats[int(poscutoff):]
-# print('train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats)))
+# negbar = progressbar.ProgressBar(maxval=len(negids), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+# negbar.start()
+# index = 0
+# for i in negids:
+#     negbar.update(index + 1)
+#     for j in movie_reviews.words(fileids=[i]):
+#         is_valid_vocab = preprocessing(j)
+#         if is_string_not_empty(is_valid_vocab):
+#             if j not in vocabulary:
+#                 vocabulary[j] = j
+#     index += 1
 #
-# classifier = NaiveBayesClassifier.train(trainfeats)
-# print('accuracy:', nltk.classify.util.accuracy(classifier, testfeats))
-# classifier.show_most_informative_features()
+# print("\n Done added negative movie vocabulary : ", len(vocabulary))
+# print("\n")
 #
-# test_sentence = "This best movie ever!!"
-# featurized_test_sentence = {i: (i in word_tokenize(test_sentence)) for i in vocabulary}
+# posbar = progressbar.ProgressBar(maxval=len(posids), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+# posbar.start()
+# index = 0
+# for i in posids:
+#     posbar.update(index + 1)
+#     for j in movie_reviews.words(fileids=[i]):
+#         is_valid_vocab = preprocessing(j)
+#         if is_string_not_empty(is_valid_vocab):
+#             if j not in vocabulary:
+#                 vocabulary[j] = j
+#     index += 1
 #
-# print("example:", test_sentence)
-# print("label:", classifier.classify(featurized_test_sentence))
+# print("\n Done added positive movie vocabulary : ", len(vocabulary))
+# print("\n")
+#
+# db_count = 64757
+# dbbar = progressbar.ProgressBar(maxval=db_count, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+# dbbar.start()
+# index = 0
+# for i in cur:
+#     dbbar.update(index + 1)
+#     words = word_tokenize(i[0])
+#     for j in words:
+#         is_valid_vocab = preprocessing(j)
+#         if is_string_not_empty(is_valid_vocab):
+#             if j not in vocabulary:
+#                 vocabulary[j] = j
+#     index += 1
+#
+# print("\n Done added database movie vocabulary : ", len(vocabulary))
+# print("\n")
+#
+# cur.close()
+# conn.close()
+#
+# with open("vocabulary.pickle", "wb") as handle:
+#     cPickle.dump(vocabulary, handle)
+
+def local_vocabulary():
+    with open('vocabulary.pickle', 'rb') as handle:
+        return cPickle.load(handle)
+
+loc_vocabulary = local_vocabulary()
+
+negcutoff = len(negfeats) * 3 / 4
+poscutoff = len(posfeats) * 3 / 4
+
+trainfeats = negfeats[:int(negcutoff)] + posfeats[:int(poscutoff)]
+testfeats = negfeats[int(negcutoff):] + posfeats[int(poscutoff):]
+print('train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats)))
+
+classifier = NaiveBayesClassifier.train(trainfeats)
+print('accuracy:', nltk.classify.util.accuracy(classifier, testfeats))
+classifier.show_most_informative_features()
+
+test_sentence = "I love it. It behaves exactly like youd expect. Plus I love the inter-app communication (the fact that I can click a follow button in the gmail app and be taken straight into the twitter app). Good job guys."
+featurized_test_sentence = {i: (i in word_tokenize(test_sentence)) for i in loc_vocabulary}
+
+print("example:", test_sentence)
+print("label:", classifier.classify(featurized_test_sentence))
