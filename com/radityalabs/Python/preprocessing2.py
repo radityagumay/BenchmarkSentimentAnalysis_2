7b536a1f9719118c2bc8df5a50019348bf78aa20
radityagumay <gumay.raditya@yahoo.com>
2017-04-03 11:20:41 +0700

    add coroutine

--- com/radityalabs/Python/preprocessing2.py
+++ com/radityalabs/Python/preprocessing2.py
@@ -3,6 +3,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import EnglishStemmer
 from nltk.corpus import stopwords
 from multiprocessing import Pool
+import asyncio
 import progressbar
 import _pickle as cPickle
 import sys, unicodedata
@@ -50,7 +51,8 @@ posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posid
 
 db_count = 52150
 
-async def calc_both_neg_and_pos():
+@asyncio.coroutine
+def calc_both_neg_and_pos():
     print("\n Initialize local sentiment review")
     initbar = progressbar.ProgressBar(maxval=db_count, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
     initbar.start()
@@ -65,34 +67,22 @@ async def calc_both_neg_and_pos():
             posfeats.append((word_feats(word_tokenize(i[0])), 'pos'))
     initbar.finish()
 
-def calc_both_neg_and_pos_callback():
-    print("worker local sentiment review is done")
-
-## do asyncTask ##
-p1 = Pool(processes = 1)
-r1 = p1.apply_async(calc_both_neg_and_pos, callback=calc_both_neg_and_pos_callback)
-r1.wait()
-## done ##
+asyncio.get_event_loop().run_until_complete(calc_both_neg_and_pos())
 
 ## save negfeats and posfeats to file
-async def save_negfeats_and_posfeats():
+@asyncio.coroutine
+def save_negfeats_and_posfeats():
     with open("negfeats.pickle", "wb") as handle:
         cPickle.dump(negfeats, handle)
     with open("posfeats.pickle", "wb") as handle:
         cPickle.dump(posfeats, handle)
 
-def save_negfeats_and_posfeats_callback():
-    print("worker negfeats and posfeats are done")
-
-## do asyncTask ##
-p2 = Pool(processes = 1)
-r2 = p2.apply_async(save_negfeats_and_posfeats, callback=save_negfeats_and_posfeats_callback)
-r2.wait()
-## done ##
+asyncio.get_event_loop().run_until_complete(save_negfeats_and_posfeats())
 
 vocabulary = {}
 
-async def running_sentiment_review_negative():
+@asyncio.coroutine
+def running_sentiment_review_negative():
     print("Initialize sentiment review negative")
     negbar = progressbar.ProgressBar(maxval=len(negids), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
     negbar.start()
@@ -109,16 +99,10 @@ async def running_sentiment_review_negative():
     print("\n Done added negative movie vocabulary : ", len(vocabulary))
     print("\n")
 
-def running_sentiment_review_negative_callback():
-    print("worker sentiment review negative is done")
+asyncio.get_event_loop().run_until_complete(save_negfeats_and_posfeats())
 
-## do asyncTask ##
-p3 = Pool(processes = 1)
-r3 = p3.apply_async(running_sentiment_review_negative, callback=running_sentiment_review_negative_callback)
-r3.wait()
-## done ##
-
-async def running_sentiment_review_positive():
+@asyncio.coroutine
+def running_sentiment_review_positive():
     print("Initialize sentiment review positive")
     posbar = progressbar.ProgressBar(maxval=len(posids), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
     posbar.start()
@@ -135,16 +119,10 @@ async def running_sentiment_review_positive():
     print("\n Done added positive movie vocabulary : ", len(vocabulary))
     print("\n")
 
-def running_sentiment_review_positive_callback():
-    print("worker sentiment review positive is done")
+asyncio.get_event_loop().run_until_complete(save_negfeats_and_posfeats())
 
-## do asyncTask ##
-p4 = Pool(processes = 1)
-r4 = p4.apply_async(running_sentiment_review_positive, callback=running_sentiment_review_positive_callback)
-r4.wait()
-## done ##
-
-async def running_db_sentiment():
+@asyncio.coroutine
+def running_db_sentiment():
     print("Initialize sentiment review database")
     dbbar = progressbar.ProgressBar(maxval=db_count, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
     dbbar.start()
@@ -162,31 +140,20 @@ async def running_db_sentiment():
     print("\n Done added database movie vocabulary : ", len(vocabulary))
     print("\n")
 
-def running_db_sentiment_callback():
-    print("worker database is done")
+def close_connection():
+    cur.close()
+    conn.close()
 
-## do asyncTask ##
-p5 = Pool(processes = 1)
-r5 = p5.apply_async(running_db_sentiment, callback=running_db_sentiment_callback)
-r5.wait()
-## done ##
+loop = asyncio.get_event_loop()
+loop.run_until_complete(running_db_sentiment())
+loop.call_soon_threadsafe(callback=close_connection)
 
-cur.close()
-conn.close()
-
-async def save_vocabulary_pickle():
+@asyncio.coroutine
+def save_vocabulary_pickle():
     with open("vocabulary.pickle", "wb") as handle:
         cPickle.dump(vocabulary, handle)
 
-def save_vocabulary_pickle_callback():
-    print("worker vocabulary pickle is done")
-
-## do asyncTask ##
-p6 = Pool(processes = 1)
-r6 = p6.apply_async(save_vocabulary_pickle, callback=save_vocabulary_pickle_callback)
-r6.wait()
-## done ##
-
+asyncio.get_event_loop().run_until_complete(save_vocabulary_pickle())
 
 # def local_vocabulary():
 #     with open('vocabulary.pickle', 'rb') as handle:
