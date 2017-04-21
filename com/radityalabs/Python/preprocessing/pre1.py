82a1adb2331b1d6fd3365b7e2a4eb68a425a72c2
radityagumay <gumay.raditya@yahoo.com>
2017-04-21 21:18:51 +0700

    fix sklearn joblib

--- com/radityalabs/Python/preprocessing/pre1.py
+++ com/radityalabs/Python/preprocessing/pre1.py
@@ -1,96 +1,85 @@
+# http://stevenloria.com/how-to-build-a-text-classification-system-with-python-and-textblob/
 from nltk.corpus import movie_reviews
-from nltk.tokenize import word_tokenize
+from textblob.classifiers import NaiveBayesClassifier
 from nltk.stem.snowball import EnglishStemmer
 from nltk.corpus import stopwords
-from itertools import chain
-import asyncio
-import progressbar
-import _pickle as cPickle
+from nltk.tokenize import word_tokenize
 import sys, unicodedata
-import pymysql
+from sklearn.externals import joblib
+from textblob import TextBlob
+import _pickle as cPickle
 import os
+import pymysql
+import asyncio
 
-conn = pymysql.connect(
-    host='127.0.0.1',
-    user='root', passwd='',
-    db='sentiment_analysis')
-
-# initialize
-cur = conn.cursor()
-cur.execute("SELECT reviewBody, label FROM sentiment_analysis.review_label_benchmark_with_polarity where (label = 'neg' or label = 'pos') and length(reviewBody) > 30 limit 0, 34636")
-stemmer = EnglishStemmer()
-tbl = dict.fromkeys(i for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P'))
+# instantiate
+# connection = pymysql.connect(
+#     host='127.0.0.1',
+#     user='root', passwd='',
+#     db='sentiment_analysis')
+#
+# cursor = connection.cursor()
+# cursor.execute("SELECT reviewBody, label FROM sentiment_analysis.review_label_benchmark_with_polarity where length(reviewBody) > 30 and (label = 'pos' or label = 'neg') limit 0, 34636")
+#
+# # variables
 path = os.path.expanduser("~/Python/SamplePython3/com/radityalabs/")
+#
+# items_train_count = 34636
+# items_test_count = 2317
+#
+# # train
+# @asyncio.coroutine
+# def load_train():
+#     train = []
+#     for item in cursor:
+#         train.append((item[0], item[1]))
+#     return train
+# train = asyncio.get_event_loop().run_until_complete(load_train())
+# print(train)
+#
+# # test
+# cursor.execute("SELECT reviewBody, label FROM google_play_crawler.authors17 where length(reviewBody) > 30 and (label = 'pos' or label = 'neg') limit 0, 2317")
+# @asyncio.coroutine
+# def load_test():
+#     test = []
+#     for item in cursor:
+#         test.append((item[0], item[1]))
+#     return test
+# test = asyncio.get_event_loop().run_until_complete(load_test())
+# print(test)
+#
+# @asyncio.coroutine
+# def save_train():
+#     with open(path + "/Python/bimbingan_data/train_twitter_corpus_34636.pickle", "wb") as handle:
+#         cPickle.dump(train, handle)
+#         print("Saving train is done")
+#
+# @asyncio.coroutine
+# def save_test():
+#     with open(path + "/Python/bimbingan_data/train_twitter_corpus_2317.pickle", "wb") as handle:
+#         cPickle.dump(test, handle)
+#         print("Saving train is done")
+#
+# asyncio.get_event_loop().run_until_complete(save_train())
+# asyncio.get_event_loop().run_until_complete(save_test())
+#
+# cursor.close()
+# connection.close()
+#
+def end_word_extractor(document):
+    tokens = document.split()
+    first_word, last_word = tokens[0], tokens[-1]
+    feats = {}
+    feats["first({0})".format(first_word)] = True
+    feats["last({0})".format(last_word)] = False
+    return feats
+#
+# cl = NaiveBayesClassifier(train, feature_extractor=end_word_extractor)
+# joblib.dump(cl, path + '/Python/bimbingan_data/sklearn-joblib-train.pkl')
 
-# variables
-db_count = 34636
-negfeats = []
-posfeats = []
-vocabulary = []
-sentence = []
-
-# functions
-def word_feats(words):
-    return dict([(word, True) for word in words])
-
-def close_connection():
-    cur.close()
-    conn.close()
-
-def word_feats(words):
-    return dict([(word, True) for word in words])
-
-@asyncio.coroutine
-def running_db_sentiment():
-    pBar1 = progressbar.ProgressBar(maxval=db_count, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-    pBar1.start()
-    index = 0
-    print("populate sentence")
-    for i in cur:
-        pBar1.update(index)
-        index += 1
-        label = i[1]
-        if label == 'neg':
-            negfeats.append((word_feats(word_tokenize(i[0])), 'neg'))
-            vocabulary.append((i[0], label))
-            sentence.append((i[0], 'neg'))
-        else:
-            posfeats.append((word_feats(word_tokenize(i[0])), 'pos'))
-            vocabulary.append((i[0], label))
-            sentence.append((i[0], 'pos'))
-    close_connection()
-
-asyncio.get_event_loop().run_until_complete(running_db_sentiment())
-
-@asyncio.coroutine
-def save_model_data():
-    with open(path + "/Python/model/model1.pickle", "wb") as handle:
-        cPickle.dump(sentence, handle)
-        print("Saving model is done")
-
-asyncio.get_event_loop().run_until_complete(save_model_data())
-
-@asyncio.coroutine
-def save_negfeats():
-    with open(path + "/Python/negfeats/negfeats1.pickle", "wb") as handle:
-        cPickle.dump(negfeats, handle)
-        print("Saving pickle negfeats is done")
-
-asyncio.get_event_loop().run_until_complete(save_negfeats())
-
-@asyncio.coroutine
-def save_posfeats():
-    with open(path + "/Python/posfeats/posfeats1.pickle", "wb") as handle:
-        cPickle.dump(posfeats, handle)
-        print("Saving pickle posfeats is done")
-
-asyncio.get_event_loop().run_until_complete(save_posfeats())
-
-@asyncio.coroutine
-def save_vocabulary_pickle():
-    voc = set(chain(*[word_tokenize(i[0].lower()) for i in vocabulary]))
-    with open(path + "/Python/vocabulary/vocabulary1.pickle", "wb") as handle:
-        cPickle.dump(voc, handle)
-        print("Saving pickle vocabulary is done")
-
-asyncio.get_event_loop().run_until_complete(save_vocabulary_pickle())
+sentence = "this app is very bad"
+cl = joblib.load(path + '/Python/bimbingan_data/sklearn-joblib-train.pkl')
+print(cl.classify(sentence))
+# blob = TextBlob(sentence, classifier=cl)
+# print(blob.classify())
+# print("Accuracy: {0}".format(cl2.accuracy(test)))
