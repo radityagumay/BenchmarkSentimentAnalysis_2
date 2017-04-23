8c9a03cfd4223ebd6d25ce7d213984f4f5de8acd
radityagumay <gumay.raditya@yahoo.com>
2017-04-23 17:36:28 +0700

    fix scenario 2 nltk corpus

--- com/radityalabs/Python/preprocessing/pre2.py
+++ com/radityalabs/Python/preprocessing/pre2.py
@@ -1,6 +1,7 @@
 # http://stevenloria.com/how-to-build-a-text-classification-system-with-python-and-textblob/
 from textblob.classifiers import NaiveBayesClassifier
 from sklearn.externals import joblib
+from textblob import TextBlob
 from nltk.corpus import movie_reviews
 import _pickle as cPickle
 import random
@@ -19,14 +20,17 @@ def end_word_extractor(document):
     return feats
 
 def save_train(train):
-    with open(path + "/Python/bimbingan_data/train_nltk_corpus_2.pickle", "wb") as handle:
+    with open(path + "/Python/bimbingan_data/nltk_train_1500_2.pickle", "wb") as handle:
         cPickle.dump(train, handle)
-        print("Saving train is done")
+        print("saving train data's is done")
 
 def save_test(test):
-    with open(path + "/Python/bimbingan_data/test_nltk_corpus_2.pickle", "wb") as handle:
+    with open(path + "/Python/bimbingan_data/nltk_test_500_2.pickle", "wb") as handle:
         cPickle.dump(test, handle)
-        print("Saving test is done")
+        print("saving test data's is done")
+
+def sentence():
+    return "This app is never good enough"
 
 def run():
     negids = movie_reviews.fileids('neg')
@@ -41,7 +45,12 @@ def run():
     test_data = collection[1500:]
     save_train(train_data)
     save_test(test_data)
-    cl = NaiveBayesClassifier(train_data)
-    joblib.dump(cl, path + '/Python/bimbingan_data/sklearn-joblib-train-nltk-2.pkl')
+
+    cl = NaiveBayesClassifier(train_data, feature_extractor=end_word_extractor)
+    blob = TextBlob(sentence(), classifier=cl)
+    print(blob.classify())
+    print("Accuracy: {0}".format(cl.accuracy(test_data)))
+    #cl = NaiveBayesClassifier(train_data)
+    #joblib.dump(cl, path + '/Python/bimbingan_data/sklearn-joblib-train-nltk-2.pkl')
 
 run()
