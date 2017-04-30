e2806682f9ba43fc1d13b9082c2067a2c6941fc8
radityagumay <gumay.raditya@yahoo.com>
2017-04-30 22:38:54 +0700

    add tfidf

--- com/radityalabs/Python/tfidf/experiment/tfidf3.py
+++ com/radityalabs/Python/tfidf/experiment/tfidf3.py
@@ -10,13 +10,14 @@ class tfidf:
     def __init__(self):
         self.documents = []
         self.query = ""
+        self.tablePunctuation = dict.fromkeys(i for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P'))
 
     def addDocument(self, document):
         self.documents.append(document)
 
     def addDocuments(self, documents):
         for document in documents:
-            tokens = word_tokenize(document)
+            tokens = word_tokenize(document[1])
             new_doc = ""
             for token in tokens:
                 if len(token) > 3:
@@ -24,14 +25,27 @@ class tfidf:
                     punct = stem.translate(tbl)
                     if punct is not None:
                         stop = punct not in set(stopwords.words('english'))
-                        if stop:  # only true we append
+                        if stop:
                             new_doc += str(punct) + " "
             self.documents.append(new_doc)
-        #self.documents = documents
-        print(self.documents)
+        self.documents = documents
 
     def addDocumentQuery(self, query):
         self.query = query
+
+    def calcTfIdf(self):
+        print("Query : ", self.query)
+
+    def simpleSplit(self, document):
+        return document.lower().split(None)
+
+    def calcTermFrequency(self):
+        for document in self.documents:
+            tokens = self.simpleSplit(document[1])
+            newTokens = []
+            for token in tokens:
+                newTokens.append(token.translate(self.tablePunctuation))
+
 
 tfidf = tfidf()
 document_query = "China has shinzo abe"
@@ -43,10 +57,10 @@ document_tokens5 = "What's the future of Abenomics? We asked Shinzo Abe for his 
 document_tokens6 = "Obama has eased sanctions on Cuba while accelerating those against the Russian Economy, even as the Ruble's value falls almost daily."
 document_tokens7 = "Vladimir Putin is riding a horse while hunting deer. Vladimir Putin always seems so serious about things - even riding horses. Is he crazy?"
 
-documents = [document_tokens1, document_tokens2, document_tokens3, document_tokens4,
-             document_tokens5, document_tokens6, document_tokens7]
+documents = [(1, document_tokens1), (2, document_tokens2), (3, document_tokens3), (4, document_tokens4),
+             (5, document_tokens5), (6, document_tokens6), (7, document_tokens7)]
 
 tfidf.addDocuments(documents)
 tfidf.addDocumentQuery(document_query)
-
+tfidf.calcTermFrequency()
 
