c43010aa5886189b22b108eddc0f404bfc4f53d3
radityagumay <gumay.raditya@yahoo.com>
2017-05-12 11:05:11 +0700

    add knn corpus

---
+++ com/radityalabs/Python/tfidf/experiment/tfidf7knn-tfidf-corpus-2.py
@@ -0,0 +1,125 @@
+# https://gist.githubusercontent.com/anabranch/48c5c0124ba4e162b2e3/raw/6b1acf8391b15ad3a663beb7e685e6835c964036/tfpdf.py
+# http://www.cs.duke.edu/courses/spring14/compsci290/assignments/lab02.html
+from __future__ import division
+from nltk.tokenize import word_tokenize
+from scipy.spatial import distance
+from nltk.stem.porter import *
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+import string
+import operator
+import csv
+import random
+import _pickle as cPickle
+import sys, unicodedata, os
+import math
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+
+iris = datasets.load_iris()
+
+# X = iris.data
+# y = iris.target
+#
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
+#
+# my_classifier = KNeighborsClassifier()
+# my_classifier.fit(X_train, y_train)
+#
+# predictions = my_classifier.predict(X_test)
+#
+# print(my_classifier.predict([[6.7, 3.1, 5.6, 2.4]]))
+# print(accuracy_score(y_test, predictions))
+
+path = os.path.expanduser("~/Python/SamplePython3/com/radityalabs/")
+
+class Similarity:
+    def __init__(self):
+        self.stemmer = PorterStemmer()
+        self.tablePunctuation = dict.fromkeys(
+            i for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P'))
+
+    def term_frequency(self, term, tokenized_document):
+        return tokenized_document.count(term)
+
+    def inverse_document_frequencies(self, tokenized_documents):
+        idf_values = {}
+        all_tokens_set = set([item for sublist in tokenized_documents for item in sublist])
+        all_tokens_set = sorted(all_tokens_set)
+        for tkn in all_tokens_set:
+            contains_token = map(lambda doc: tkn in doc, tokenized_documents)
+            idf_values[tkn] = 1 + math.log(len(tokenized_documents) / (sum(contains_token)))
+        sorted_idf_values = {}
+        for key in sorted(idf_values):
+            sorted_idf_values[key] = idf_values[key]
+        print(all_tokens_set)
+        return sorted_idf_values
+
+    def save_tfidf_150_documents(self, tfidf):
+        with open(path + "/Python/bimbingan_data/knn/tfidf-150-documents.pickle", "wb") as handle:
+            cPickle.dump(tfidf, handle)
+
+    # 1. preprocessing documents
+    def load_documents(self):
+        with open(path + "/Python/bimbingan_data/tfidf-final-corpus-preprocessing-document.pickle", "rb") as handle:
+            return cPickle.load(handle)
+
+    # 2. manually label
+    def load_document_with_categorized_label(self):
+        with open(path + "/Python/bimbingan_data/tfidf-final-corpus-2-with-sentiment-category.csv", 'r') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            collection = []
+            index = 0
+            for row in spamreader:
+                if index != 0:
+                    collection.append(', '.join(row))
+                index += 1
+            return collection
+
+    # 3. calc tfidf
+    def tfidf(self):
+        tokenized_documents = []
+        documents = self.load_document_with_categorized_label()
+        for document in documents:
+            doc = document.split(",")
+            tokens = word_tokenize(doc[0])
+            tokenized_documents.append(tokens)
+
+        idf = self.inverse_document_frequencies(tokenized_documents)
+        tfidf_documents = []
+        doc_index = 0
+        for document in tokenized_documents:
+            doc_tfidf = []
+            for term in idf.keys():
+                tf = self.term_frequency(term, document)
+                doc_tfidf.append(tf * idf[term])
+            doc = documents[doc_index].split(",")
+            tfidf = [doc_tfidf, doc[1], doc[2]]
+            tfidf_documents.append(tfidf)
+            doc_index += 1
+        return tfidf_documents
+
+    # 4. training and testing knn
+    def knn(self):
+        data = []
+        target = []
+
+        tfidf = self.tfidf()
+        pick_sample = tfidf[15]
+        print(pick_sample[0], pick_sample[2])
+
+
+        for item in tfidf:
+            data.append(item[0])
+            target.append(item[2])
+        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=.5)
+        my_classifier = KNeighborsClassifier()
+        my_classifier.fit(X_train, y_train)
+        print(my_classifier.predict([pick_sample[0]]))
+        # predictions = my_classifier.predict(X_test)
+        # print(data, accuracy_score(y_test, predictions))
+
+code = Similarity()
+print(code.tfidf())
